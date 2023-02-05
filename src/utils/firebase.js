32afeb92c6a633445886635dc6e0e4383ac095d1
Raul <tangarinikaorinoko@hotmail.com>
2023-02-05 14:57:49 +0100

    ConFirebase

---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC-Jdl2IirRoAJlUNfH96A76lH1zviSyZE",
+  authDomain: "curso-react-92088.firebaseapp.com",
+  projectId: "curso-react-92088",
+  storageBucket: "curso-react-92088.appspot.com",
+  messagingSenderId: "990920217511",
+  appId: "1:990920217511:web:1ffe686de7384e6e0edc82"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
